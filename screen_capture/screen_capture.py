fafae746dfbaeebe153c04f96f3a9d7a019b4353
Jae-sung Jun <40080536+Unkn03nUser@users.noreply.github.com>
2020-04-01 22:58:31 +0900

    DS3 Window Detect Update

--- screen_capture/screen_capture.py
+++ screen_capture/screen_capture.py
@@ -3,30 +3,42 @@ import mss.tools
 import time
 import cv2
 import numpy
+import pygetwindow as gw
 
 """
 with mss() as sct:
     for _ in range(100):
         sct.shot()
 """
-
-def screen_record(top, left, width, height, monitor_number):
+def getDS3Window():
+    #DARK SOULS III
+    DS3 = gw.getWindowsWithTitle('DARK SOULS III')
+    if DS3 == None:
+        DS3_window = DS3[0]
+        return DS3_window.top, DS3_window.left, DS3_window.width, DS3_window.height
+    else:
+        return 0, 0, 0, 0
+def screenRecord(monitor_number):
     with mss.mss() as sct:
         # Part of the screen to capture
-        monitor = {
-            "top": top,
-            "left": left,
-            "width": width,
-            "height": height,
-            "mon": monitor_number
-        }
-
         while "Screen capturing":
+            top, left, width, height = getDS3Window()
+            if top == 0 and left == 0 and width == 0 and height == 0:
+                print("Can't Find DS3 Window")
+                cv2.destroyAllWindows()
+                break
+            monitor = {
+                "top": top+40,
+                "left": left+40,
+                "width": width-50,
+                "height": height-50,
+                "mon": monitor_number
+            }
             last_time = time.time()
             # Get raw pixels from the screen, save it to a Numpy array
             img = numpy.array(sct.grab(monitor))
             
-            img = cv2.bitwise_not(img)
+            #img = cv2.bitwise_not(img)
 
             cv2.imshow("OpenCV/Numpy normal", img)
             # Display the picture in grayscale
@@ -39,4 +51,4 @@ def screen_record(top, left, width, height, monitor_number):
                 break
 
 #test code
-screen_record(40, 0, 300, 300, 1)
+screenRecord(1)
